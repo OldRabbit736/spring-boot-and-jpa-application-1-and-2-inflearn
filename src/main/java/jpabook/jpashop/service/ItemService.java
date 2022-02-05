1c4dafb3e59d7a4e6bf95ce6f431b3d6cd65f540
jspark <sylvan0212@gmail.com>
2022-02-05 20:11:22 +0900

    변경 감지와 병합(merge)

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,17 @@ public class ItemService {
 
     public List<Item> findItems() {
         return itemRepository.findAll();
+    }
+
+    // 이 방법(변경감지)이 merge 보다 훨씬 안전하다!! 이 방법을 쓰자!!
+    // 또한 필드마다 set으로 값을 변경하기 보다는, usecase 마다의 method를 만들어서 업데이트 하는 것이 낫다! (addStock 같은)
+    // set은 왠만해서는 쓰지 말자. // set을 없애고 변경 포인트 method를 소수로 만들어 놓으면 나중에 변경 call 한 곳을 역으로 추적하기 쉬워진다!!
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public Item findOne(Long itemId) {
